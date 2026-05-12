d30615cc4fc96e9b5955b2613967eefacdee1f96
Hernan Poblete <hernan.poblete@ug.uchile.cl>
2026-05-12 12:51:52 -0400

    refactor: Changed dashboard to use jinja template

--- src/app.py
+++ src/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
 def index():
-	return render_template("index.html")
+	return render_template("dashboard_temp.html")
 
 
 app.run(port=5000, host="0.0.0.0", debug=True, threaded=True)
