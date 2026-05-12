a65cf697aae9a1f88a993d0c8c29622b7fc078ea
Hernan Poblete <hernan.poblete@ug.uchile.cl>
2026-05-12 12:44:42 -0400

    feat: Created Flask app

---
+++ src/app.py
@@ -0,0 +1,12 @@
+from flask import Flask, url_for, render_template
+
+
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+	return render_template("index.html")
+
+
+app.run(port=5000, host="0.0.0.0", debug=True, threaded=True)
