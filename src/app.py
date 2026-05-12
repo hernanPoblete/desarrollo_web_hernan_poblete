7232dc391d65d6399e1435ca5e143ecfd64a2e53
Hernan Poblete <hernan.poblete@ug.uchile.cl>
2026-05-12 14:00:29 -0400

    feat: Adds endpoints to compy with project requirements

--- src/app.py
+++ src/app.py
@@ -1,12 +1,23 @@
-from flask import Flask, url_for, render_template
+from flask import Flask, url_for, render_template, request
 
 
 
 app = Flask(__name__)
 
-@app.route("/", methods=["GET", "POST"])
+@app.route("/", methods=["GET"])
 def index():
-	return render_template("dashboard_temp.html")
+	return render_template("dashboard.html")
 
+
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+	if request.method == "GET":
+		return render_template("register.html")
+	
+
+@app.route("/members", methods=["GET"])
+def members():
+	return render_template("dashboard.html")
 
 app.run(port=5000, host="0.0.0.0", debug=True, threaded=True)
