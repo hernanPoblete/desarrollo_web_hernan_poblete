a0f6ba13c033eda28cea8da407ce80b033373e0d
Hernan Poblete <hernan.poblete@ug.uchile.cl>
2026-06-12 22:42:34 -0400

    feat: Added backend logic for activity

--- src/routes/register/register.py
+++ src/routes/register/register.py
@@ -1,7 +1,8 @@
 from flask import Blueprint, request, render_template, redirect, make_response
-from utils.db import db, comuna, region, miembro
+from utils.db import db, comuna, region, miembro, actividad
 from datetime import datetime
 from .validators import validate_member, ValidationError
+import os
 
 register_bp = Blueprint('register', __name__, url_prefix='/register')
 
@@ -40,11 +41,41 @@ def register_member_route():
 			return make_response(render_template("register.html", regiones = qreg, error_msg=e.reason), e.status)
 
 		
-		return redirect('/members')
+		return redirect('/register/activity')
 
 
 
 @register_bp.route("/activity", methods = ["GET", "POST"])
 def register_activity_route():
 
-	return render_template("register_activity.html")
+	if request.method == 'GET':
+		return render_template("register_activity.html")
+
+	else:
+
+		q = request.form.get
+
+		stmt = actividad.insert().values(
+			nombre=q('nombre'),
+			dia=q('horario'),
+			tipo=q('tipo_actividad'),
+			miembro_id = q('rut').replace('.', '').replace('-', '')[0:-1],
+			duracion = q('duracion'),
+			descripcion=q('descripcion'),
+			hora_inicio = q("horaInicio")
+		)
+
+
+		result = db.session.execute(stmt)
+
+		folder = result.inserted_primary_key[0]
+		nombre_carpeta = 'src/static/uploads/'+str(folder)
+		os.makedirs(nombre_carpeta, exist_ok=True)
+		for file in request.files.getlist('fotos'):
+
+			file.save(nombre_carpeta+'/'+file.filename)
+
+
+		
+
+		return redirect('/?s_msg="Actividad Creada con éxito"')
